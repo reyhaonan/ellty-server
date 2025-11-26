17dc5f898f8cee7527bd2292c12d4def4a3431b3
reyhaonan <reyhaonan2003@gmail.com>
2025-11-26 23:52:34 +0700

    chore: none samesite

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      sameSite: "strict",
+      sameSite: "none",
       httpOnly: true,
       secure: env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24, // 1 day
